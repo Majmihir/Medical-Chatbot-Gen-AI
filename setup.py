9815fd56f946fc95aec2d46570ff31c31b9e9bbf
Mihir Raj <rajmihir26@gmail.com>
2025-02-07 03:44:04 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.1',
+    author= 'Mihir Raj',
+    author_email= 'rajmihr26@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
